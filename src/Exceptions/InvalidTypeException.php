4f1821960a49b302bc1729e9850506338128a360
Yuri Chandra <yurichandra@gmail.com>
2019-02-06 23:11:21 +0700

    Added exceptions

---
+++ src/Exceptions/InvalidTypeException.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Maker\Exceptions;
+
+class InvalidTypeException extends \Exception
+{
+    protected $message = 'Your character type was invalid';
+}
---
+++ src/Exceptions/NotEvenException.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Maker\Exceptions;
+
+class NotEvenException extends \Exception
+{
+    protected $message = 'Your length was not even number';
+}
